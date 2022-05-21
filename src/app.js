8434cef8c11996f4d14eee62a170468cb42646a0
duo <hypersonolabs@gmail.com>
2022-05-21 16:26:42 +0900

    remove apollo server option and integrated into express js server

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const { ApolloServer } = require("apollo-server");
+const { graphqlHTTP } = require("express-graphql");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+
 const app = express();
 const port = 8000;
 
@@ -7,10 +9,15 @@ const port = 8000;
 const { typeDefs } = require("./schema");
 const { resolvers } = require("./resolvers");
 
-//create apollo server
-const server = new ApolloServer({ typeDefs, resolvers });
+const schema = makeExecutableSchema({ resolvers, typeDefs });
 
-//initiate server
-server
-  .listen(port, () => console.log(`server is listening ${port}`))
-  .then(({ url }) => console.log(`Server is running on ${url}`));
+//graphql routes
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
+
+app.listen(port, () => console.log(`server is listening at ${port}`));
